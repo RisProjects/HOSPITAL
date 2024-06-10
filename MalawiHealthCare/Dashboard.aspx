dd16115af04c91c0a3049730a9818f88ea0d895b
Ris <appalarajuk383@gmail.com>
2024-06-10 15:48:08 +0530

    added default pages

---
+++ MalawiHealthCare/Dashboard.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/HMSMaster.Master" AutoEventWireup="true" CodeBehind="Dashboard.aspx.cs" Inherits="HMSMalawi.Dashboard" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+  <h2>Welcome</h2>
+
+</asp:Content>
---
+++ MalawiHealthCare/Data_Access_Layer/Dal.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MalawiHealthCare.Data_Access_Layer
+{
+    public class Dal
+    {
+        SqlConnection con;
+        DataTable dt = new DataTable();
+
+        SqlDataAdapter da = new SqlDataAdapter();
+        SqlCommand cmd = new SqlCommand();
+        DataSet ds = new DataSet();
+        public string Authorizer = "Normal User";
+        public Dal()
+        {
+            //con = new SqlConnection(System.Configuration.ConfigurationSettings.AppSettings.Get("connection"));
+
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["Dbconnection"].ConnectionString);
+
+        }
+        public void ClearDataTable()
+        {
+            if (dt != null)
+            {
+                if (dt.Rows.Count > 0)
+                {
+                    dt.Rows.Clear();
+                    dt.Columns.Clear();
+                    dt.Clear();
+                }
+            }
+        }
+
+        public DataTable Fun_MenuItems(int UserId)
+        {
+
+            ClearDataTable();
+            da = new SqlDataAdapter("Exec MenuItems_P '" + UserId + "'", con);
+            da.SelectCommand.CommandTimeout = 300;
+            ds.Clear();
+            da.Fill(ds, "Menus_Users");
+            dt = ds.Tables["Menus_Users"];
+            return dt;
+        }
+
+        public DataTable Fun_AccessRights(int UserId, int MenuId, string Tran)
+        {
+
+            ClearDataTable();
+            da = new SqlDataAdapter("Exec FormAccess_P '" + UserId + "','" + MenuId + "','" + Tran + "'", con);
+            da.SelectCommand.CommandTimeout = 300;
+            ds.Clear();
+            da.Fill(ds, "Menus_Users");
+            dt = ds.Tables["Menus_Users"];
+            return dt;
+        }
+
+
+    }
+}
